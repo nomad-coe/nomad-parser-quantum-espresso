448168c831eeb85a60130d8d9ab312162b2d9988
Henning Glawe <glaweh@debian.org>
2016-07-12 16:20:31 +0200

    fix shell cut+paste error

--- src/test/scala/eu/nomad_lab/parsers/QuantamExpressoParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/QuantamExpressoParserSpec.scala
@@ -5,10 +5,10 @@ import org.specs2.mutable.Specification
 object QuantamExpressoParserSpec extends Specification {
   "QuantamExpressoParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(QuantumEspressoParser, "parsers/quantam-expresso/test/examples/PWSCF/benchmark.out.r11920.inp\=dft1.in", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(QuantumEspressoParser, "parsers/quantam-expresso/test/examples/PWSCF/benchmark.out.r11920.inp=dft1.in", "json-events") must_== ParseResult.ParseSuccess
     }
     "test with json" >> {
-      ParserRun.parse(QuantumEspressoParser, "parsers/quantam-expresso/test/examples/PWSCF/benchmark.out.r11920.inp\=dft1.in", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(QuantumEspressoParser, "parsers/quantam-expresso/test/examples/PWSCF/benchmark.out.r11920.inp=dft1.in", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
