a521b08d7872a9ca448e53f05559d70b68d3ad5a
Henning Glawe <glaweh@debian.org>
2016-07-13 09:49:55 +0200

    fix more 'creative spelling' of quantum espresso

--- src/test/scala/eu/nomad_lab/parsers/QuantumEspressoParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/QuantumEspressoParserSpec.scala
@@ -5,10 +5,10 @@ import org.specs2.mutable.Specification
 object QuantumEspressoParserSpec extends Specification {
   "QuantumEspressoParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(QuantumEspressoParser, "parsers/quantam-expresso/test/examples/PWSCF/benchmark.out.r11920.inp=dft1.in", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(QuantumEspressoParser, "parsers/quantum-espresso/test/examples/PWSCF/benchmark.out.r11920.inp=dft1.in", "json-events") must_== ParseResult.ParseSuccess
     }
     "test with json" >> {
-      ParserRun.parse(QuantumEspressoParser, "parsers/quantam-expresso/test/examples/PWSCF/benchmark.out.r11920.inp=dft1.in", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(QuantumEspressoParser, "parsers/quantum-espresso/test/examples/PWSCF/benchmark.out.r11920.inp=dft1.in", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
