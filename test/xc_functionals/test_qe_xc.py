c68f865c3c62a5d9abe6cef19698ce9471821a90
Henning Glawe <glaweh@debian.org>
2016-07-21 18:14:23 +0200

    use pprint in xc_functional tester

--- test/xc_functionals/test_qe_xc.py
+++ test/xc_functionals/test_qe_xc.py
@@ -4,18 +4,25 @@ from QuantumEspressoXC import translate_qe_xc_num
 import sys
 import logging
 import re
+import pprint
 
 
 LOGGER = logging.getLogger(__name__)
+PP = pprint.PrettyPrinter(indent=2, width=190)
 
 
 def process_line(line):
     lr = line.rstrip()
     m = re.match(r".*\(([^\)]+)\)",lr)
-    if m:
-        translate_qe_xc_num(m.group(1))
-    else:
+    if m is None:
         LOGGER.error("unrecognized line: %s",lr)
+        return None
+    qe_xc=translate_qe_xc_num(m.group(1))
+    if qe_xc is None:
+        print("qe_xc: %s\nNone" % (lr))
+    else:
+        print(("qe_xc: %s\n" % (lr)) + PP.pprint(qe_xc))
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
