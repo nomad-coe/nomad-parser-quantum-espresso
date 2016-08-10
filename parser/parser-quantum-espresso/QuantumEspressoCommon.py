ae9a20e7745ba0f2c58ec7fff8ef621e6b1ee283
Henning Glawe <glaweh@debian.org>
2016-08-10 13:14:48 +0200

    coverageIgnore for SMOOTH->DENSE info msg

--- parser/parser-quantum-espresso/QuantumEspressoCommon.py
+++ parser/parser-quantum-espresso/QuantumEspressoCommon.py
@@ -106,6 +106,7 @@ class ParserQuantumEspresso(object):
             r"\s*Diagonalizing with lowered threshold\s*$",
             # initialization info
             r"\s*Initializing real-space augmentation for DENSE grid\s*$",
+            r"\s*SMOOTH grid -> DENSE grid\s*$",
             r"\s*Checking if some PAW data can be deallocated\.\.\.\s*$",
             r"\s*ierr\s*=\s*\d+",
             # table separators
