69b9dc1fb84dbe7610a223f57168031c43a1c915
Henning Glawe <glaweh@debian.org>
2017-11-29 19:43:15 +0100

    assume LDA_X to be part of GGA_X_RPW86

--- parser/parser-quantum-espresso/QuantumEspressoXC.py
+++ parser/parser-quantum-espresso/QuantumEspressoXC.py
@@ -567,6 +567,9 @@ EXCHANGE_GRADIENT_CORRECTION = [
         'xc_terms': [{
             'XC_functional_name': "GGA_X_RPW86",
         }],
+        'xc_terms_subtract': [{
+            'XC_functional_name': 'LDA_X',
+         }],
         'xc_section_method': {
             'x_qe_xc_igcx_name':       "rw86",
             'x_qe_xc_igcx_comment':    "revised PW86",
