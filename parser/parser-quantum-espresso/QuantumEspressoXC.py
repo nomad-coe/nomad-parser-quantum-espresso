ef04757d67a39646753eb34ac39d60e3363a5f54
Henning Glawe <glaweh@debian.org>
2017-11-29 18:58:56 +0100

    remove LDA_X from HSE06

--- parser/parser-quantum-espresso/QuantumEspressoXC.py
+++ parser/parser-quantum-espresso/QuantumEspressoXC.py
@@ -554,6 +554,9 @@ EXCHANGE_GRADIENT_CORRECTION = [
         'xc_terms': [{
             'XC_functional_name': "HYB_GGA_XC_HSE06",
         }],
+        'xc_terms_subtract': [{
+            'XC_functional_name': 'LDA_X',
+         }],
         'xc_section_method': {
             'x_qe_xc_igcx_name':       "hse",
             'x_qe_xc_igcx_comment':    "HSE screened exchange",
