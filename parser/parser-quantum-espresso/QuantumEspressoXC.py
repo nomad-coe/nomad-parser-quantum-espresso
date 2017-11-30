d4db3123b1b3d303a4559dc33915e20ee6dbbdba
Henning Glawe <glaweh@debian.org>
2017-11-30 04:19:18 +0100

    remove LDA_X from sogga

--- parser/parser-quantum-espresso/QuantumEspressoXC.py
+++ parser/parser-quantum-espresso/QuantumEspressoXC.py
@@ -621,6 +621,9 @@ EXCHANGE_GRADIENT_CORRECTION = [
         'xc_terms': [{
             'XC_functional_name': "GGA_X_SOGGA",
         }],
+        'xc_terms_subtract': [{
+            'XC_functional_name': 'LDA_X',
+         }],
         'xc_section_method': {
             'x_qe_xc_igcx_name':       "sox",
             'x_qe_xc_igcx_comment':    "sogga",
