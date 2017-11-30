b4a029acb049a9996d48312a167670ffb9c4e71e
Henning Glawe <glaweh@debian.org>
2017-11-30 07:21:53 +0100

    remove LDA_C_PW from ggc/pw91

--- parser/parser-quantum-espresso/QuantumEspressoXC.py
+++ parser/parser-quantum-espresso/QuantumEspressoXC.py
@@ -808,6 +808,9 @@ CORRELATION_GRADIENT_CORRECTION = [
         'xc_terms': [{
             'XC_functional_name': "GGA_C_PW91",
         }],
+        'xc_terms_subtract': [{
+            'XC_functional_name': 'LDA_C_PW',
+        }],
         'xc_section_method': {
             'x_qe_xc_igcc_name':       "ggc",
             'x_qe_xc_igcc_comment':    "Perdew-Wang 91 corr.",
