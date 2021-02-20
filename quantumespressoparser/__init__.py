c2d62eed57b7845b42b98ba5d44578071a3a0cfd
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-02-20 01:39:36 +0100

    Implemented new parser based on TextParser

--- quantumespressoparser/__init__.py
+++ quantumespressoparser/__init__.py
@@ -12,4 +12,4 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from quantumespressoparser.parser_quantum_espresso import QuantumEspressoParserPWSCF
+from quantumespressoparser.quantum_espresso_parser import QuantumEspressoParser
